c82142629ca8367c890b26c00914a4a9f9de5dd6
Aliya Gamez <aliya.e.gamez@gmail.com>
2025-12-16 19:36:50 -0500

    testing git, created MediaItem class with the basic media structure

---
+++ Models/MediaItem.cs
@@ -0,0 +1,30 @@
+﻿namespace MediaCatalog.Models
+{
+    class MediaItem
+    {
+        //idk if this is neccessary, i remember it from c++?
+        public MediaItem(int id, string title, string description, int releaseYear, MediaTypes mediaTypes, string imageUrl)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            ReleaseYear = releaseYear;
+            MediaType = mediaTypes;
+            ImageUrl = imageUrl;
+        }
+        public enum MediaTypes
+        {
+            Movie,
+            Series,
+            Game,
+            Book,
+            Manga
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int ReleaseYear { get; set; }
+        public MediaTypes MediaType { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
